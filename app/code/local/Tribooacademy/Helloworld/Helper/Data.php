8c04bb0c2628094da8aae1c9712772c3d74e2284
Marco Pierozzi <marco.pierozzi@triboo.it>
2019-02-21 18:18:02 +0100

    block lesson

--- app/code/local/Tribooacademy/Helloworld/Helper/Data.php
+++ app/code/local/Tribooacademy/Helloworld/Helper/Data.php
@@ -7,4 +7,15 @@
  */ 
 class Tribooacademy_Helloworld_Helper_Data extends Mage_Core_Helper_Abstract {
 
+    public function getCustomerNameFromId($id) {
+        $name = '';
+        if($id) {
+            $customer = Mage::getModel('customer/customer')->load($id);
+            if($customer->getId()) {
+                $name = $customer->getName();
+            }
+        }
+        return $name;
+    }
+
 }
--- app/code/local/Tribooacademy/Helloworld/controllers/HelloworldController.php
+++ app/code/local/Tribooacademy/Helloworld/controllers/HelloworldController.php
@@ -7,4 +7,20 @@
  */
 class Tribooacademy_Helloworld_HelloworldController extends Mage_Core_Controller_Front_Action {
 
+    public function indexAction() {
+        var_dump($this->getLayout()->getUpdate()->getHandles());
+        var_dump($this->getFullActionName());
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+    public function toAction() {
+        $id = $this->getRequest()->getParam('id',null);
+        $helper = Mage::helper('academy');
+        $to = $helper->getCustomerNameFromId($id);
+        echo "Hello $to!";
+    }
+
+
+
 }

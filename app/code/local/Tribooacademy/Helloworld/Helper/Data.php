8f202b0ab060d7667dc0ebe37f39f3ecf523d286
Marco Pierozzi <marco.pierozzi@triboo.it>
2019-02-21 18:17:46 +0100

    first version

---
+++ app/code/local/Tribooacademy/Helloworld/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 21/02/19
+ * Time: 17.12
+ */ 
+class Tribooacademy_Helloworld_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Tribooacademy/Helloworld/controllers/HelloworldController.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 21/02/19
+ * Time: 17.17
+ */
+class Tribooacademy_Helloworld_HelloworldController extends Mage_Core_Controller_Front_Action {
+
+}

ed8a4277f1c443a628ffee29c8761fb2929962df
Marco Pierozzi <marco.pierozzi@triboo.it>
2019-02-26 19:12:10 +0100

    26-02-2019

---
+++ app/code/local/Tribooacademy/Newsletteroverlay/Block/Overlay.php
@@ -0,0 +1,4 @@
+<?php
+class Tribooacademy_Newsletteroverlay_Block_Overlay extends Mage_Core_Block_Template {
+
+}
---
+++ app/code/local/Tribooacademy/Newsletteroverlay/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 26/02/19
+ * Time: 18.25
+ */ 
+class Tribooacademy_Newsletteroverlay_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}

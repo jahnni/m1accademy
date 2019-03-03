3f69674b37e828f4e5e110b7ce1896f83ea3a144
jahnni <giovanni.sposito@gmail.com>
2019-03-03 22:01:18 +0100

    creiamo il modulo

---
+++ app/code/community/Triboo/Sales/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jahnni
+ * Date: 03/03/19
+ * Time: 21.57
+ */ 
+class Triboo_Sales_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/community/Triboo/Sales/sql/triboo_sales_setup/install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jahnni
+ * Date: 03/03/19
+ * Time: 21.57
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();

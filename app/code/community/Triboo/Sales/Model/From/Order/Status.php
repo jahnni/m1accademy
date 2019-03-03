71092a57c93462d1ca2c2a38713c34ed31e6c0c8
jahnni <giovanni.sposito@gmail.com>
2019-03-03 22:42:57 +0100

    implemented processor

--- app/code/community/Triboo/Sales/Model/From/Order/Status.php
+++ app/code/community/Triboo/Sales/Model/From/Order/Status.php
@@ -5,12 +5,29 @@
  * Date: 03/03/19
  * Time: 22.09
  */ 
-class Triboo_Sales_Model_From_Order_Status extends Mage_Core_Model_Abstract
+class Triboo_Sales_Model_From_Order_Status extends Tbuy_Flow_Model_From_Abstract
 {
 
     protected function _construct()
     {
         $this->_init('triboo_sales/from_order_status');
+    }
+
+    public function process() {
+        //loadorder
+        $order = Mage::getModel('sales/order')->loadByIncrementId($this->getIncrementId());
+        if(!$order->getId()){
+            Mage::throwException("Order with id ".$this->getIncrementId()." not found");
+        }
+        try{
+            $order->addStatusHistoryComment("Updated via flow",$this->getStatus())->save();
+            $order->save();
+        }catch (Exception $e){
+            Mage::logException($e);
+            return false;
+        }
+
+        return true;
     }
 
 }
